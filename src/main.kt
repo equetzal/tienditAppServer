fb86152e3fca1b1ed6d5d4c38425f6ff59091502
equetzal <eithnegomez@hotmail.com>
2020-06-12 22:52:46 -0500

    deleted test

--- src/main.kt
+++ src/main.kt
@@ -4,18 +4,6 @@ import com.google.gson.GsonBuilder
 fun main() {
     val servidor = server()
 
-    //servidor.db.addProduct("Cubrebocas", "QUETZ-3285TP", "./files/products/cubrebocas.png", 2300.0, 136)
-    servidor.db.newPurchase(0, mapOf(0 to 2, 2 to 2))
-    //servidor.db.removeProduct(1)
-    //servidor.db.updateProduct(3, "Tamal de Mole", 15.5, 67)
-
-    //servidor.db.saveCart(0, mapOf(3 to 10, 2 to 1, 0 to 1))
-
-    val ans = servidor.db.getCart(0)
-    val json = (GsonBuilder().setPrettyPrinting().create()).toJson(ans)
-    println("Product List= $json")
-
-
     var serverThread = Thread{
         run {
             println("Starting Server")
